d884470244f15d2304e57933ef5ffc2a66e95d87
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-22 01:02:07 +0000

    Updating to add golden path

--- lib/pages/home/home_model.dart
+++ lib/pages/home/home_model.dart
@@ -1,11 +1,22 @@
+import '/components/nav_bar_with_middle_button_widget.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import 'home_widget.dart' show HomeWidget;
 import 'package:flutter/material.dart';
 
 class HomeModel extends FlutterFlowModel<HomeWidget> {
-  @override
-  void initState(BuildContext context) {}
+  ///  State fields for stateful widgets in this page.
+
+  // Model for NavBarWithMiddleButton component.
+  late NavBarWithMiddleButtonModel navBarWithMiddleButtonModel;
 
   @override
-  void dispose() {}
+  void initState(BuildContext context) {
+    navBarWithMiddleButtonModel =
+        createModel(context, () => NavBarWithMiddleButtonModel());
+  }
+
+  @override
+  void dispose() {
+    navBarWithMiddleButtonModel.dispose();
+  }
 }
--- lib/pages/home/home_widget.dart
+++ lib/pages/home/home_widget.dart
@@ -1,7 +1,6 @@
-import '/auth/firebase_auth/auth_util.dart';
+import '/components/nav_bar_with_middle_button_widget.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
 import 'home_model.dart';
 export 'home_model.dart';
@@ -42,35 +41,40 @@ class _HomeWidgetState extends State<HomeWidget> {
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
         body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              FFButtonWidget(
-                onPressed: () async {
-                  GoRouter.of(context).prepareAuthEvent();
-                  await authManager.signOut();
-                  GoRouter.of(context).clearRedirectLocation();
-
-                  context.goNamedAuth('SignIn', context.mounted);
-                },
-                text: 'Log out',
-                options: FFButtonOptions(
-                  width: double.infinity,
-                  height: 40.0,
-                  padding: const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
-                  iconPadding:
-                      const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                  color: FlutterFlowTheme.of(context).primary,
-                  textStyle: FlutterFlowTheme.of(context).titleSmall.override(
-                        fontFamily: 'Inter',
-                        color: Colors.white,
-                        letterSpacing: 0.0,
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          'assets/images/Curb_Alert!_logo_no_bg.png',
+                          width: 20.0,
+                          height: 80.0,
+                          fit: BoxFit.contain,
+                          alignment: const Alignment(-1.0, -1.0),
+                        ),
                       ),
-                  elevation: 0.0,
-                  borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ],
                 ),
-              ),
-            ],
+                Flexible(
+                  child: Align(
+                    alignment: const AlignmentDirectional(0.0, 1.0),
+                    child: wrapWithModel(
+                      model: _model.navBarWithMiddleButtonModel,
+                      updateCallback: () => safeSetState(() {}),
+                      child: const NavBarWithMiddleButtonWidget(),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
